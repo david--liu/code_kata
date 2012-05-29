daae1016467f96782c0c5e1d79d165cb87f95868
David Liu <david.liu@huskyenergy.com>
2012-05-29 16:34:44 -0600

    string end trimmer

---
+++ code_kata/EndTrimmer/Test/EndTrimmerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace code_kata.EndTrimmer.Test
+{
+    [TestFixture]
+    public class EndTrimmerTest
+    {
+
+        [Test]
+        public void ShouldTrimEndingSpaces()
+        {
+            Assert.AreEqual("abc", EndTrimmer.Trim("abc "));
+        }
+
+        [Test]
+        public void ShouldTrimEndingTabs()
+        {
+            Assert.AreEqual("abc", EndTrimmer.Trim("abc\t"));
+        }
+
+        [Test]
+        public void ShouldNotTrimNewLine()
+        {
+            Assert.AreEqual("abc\n", EndTrimmer.Trim("abc\n"));
+        }
+
+
+        [Test]
+        public void ShouldNotTrimLeadingSpaces()
+        {
+            Assert.AreEqual(" abc", EndTrimmer.Trim(" abc"));
+        }
+
+        [Test]
+        public void ShouldNotTrimReturns()
+        {
+            Assert.AreEqual("ab\r\ncd\n", EndTrimmer.Trim( "ab\r\ncd\n"));
+            
+        }
+    }
+
+    public class EndTrimmer
+    {
+        public static string Trim(string line)
+        {
+            return line.TrimEnd(' ').TrimEnd('\t');
+        }
+    }
+}
