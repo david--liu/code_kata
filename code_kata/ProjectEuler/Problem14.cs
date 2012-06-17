3de21cc12340b0cd97ca8ab6ea972b4be062ecb5
git_name <git_email>
2012-06-17 00:11:05 -0600

    Problem 14, 15

---
+++ code_kata/ProjectEuler/Problem14.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+using System.Linq;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem14
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_iterating_collatz : concern
+        {
+            static ConcurrentDictionary<long, long> map = new ConcurrentDictionary<long, long>();
+            It should_return_10_when_starting_with_13 = () =>
+            {
+                for (int i = 2; i <= 13; i++)
+                {
+                    map.AddOrUpdate(i, 0, (x,y) => 0);
+                }
+
+                for (int i = 2; i <= 13; i++)
+                {
+                    map[i] = GetCollatzSequences(i);
+                }
+                map[13].ShouldEqual(10);
+            };
+
+            It OBSERVATION_NAME = () =>
+                GetCollatzSequences(999167);
+
+            It should_get_result = () =>
+            {
+                map.Clear();
+                for (long i = 2; i <= 1000000; i++)
+                {
+                    map.AddOrUpdate(i, 0, (x, y) => 0);
+                }
+                
+                map.Keys.AsParallel().ForAll(x => map[x] = GetCollatzSequences(x));
+
+                var value = map.Values.Max(x => x);
+                Console.Out.WriteLine(value);
+                Console.Out.WriteLine(map.First(x => x.Value == value));
+            };
+            static long GetCollatzSequences(long value)
+            {
+                int count = 0;
+                long running = value;
+
+                if (map.ContainsKey(running) && map[running] > 0)
+                {
+                    return map[running];
+                }
+
+ //               Console.Out.WriteLine(value);
+
+                while (running != 1 && running > 0)
+                {
+
+                    if (running % 2 == 0)
+                    {
+                        running /= 2;
+                    }
+                    else
+                    {
+                        running = 3*running + 1;
+                    }
+
+                    ++count;
+
+                    //if (map.ContainsKey(running))
+                    //{
+                    //    if (map[running] == 0)
+                    //    {
+                    //        map[running] = GetCollatzSequences(running);
+                    //    }
+                    //    return map[running] + count;
+
+                    //}
+
+                    if(count > 1000)
+                        Console.Out.WriteLine(value);
+
+                   }
+                return count + 1;
+            }
+        }
+    }
+}
---
+++ code_kata/ProjectEuler/Problem15.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem15
+    {
+        public abstract class concern : Observes
+        {
+        }
+
+        public class when_observation_name : concern
+        {
+            It should_return_6_for_2x2 = () =>
+            {
+                var sum = GetTotalRoute(2);
+                sum.ShouldEqual(6);
+            };
+
+            It should_return_result = () =>
+            {
+                Console.Out.WriteLine(GetTotalRoute(2));
+                Console.Out.WriteLine(GetTotalRoute(3));
+                Console.Out.WriteLine(GetTotalRoute(4));
+                Console.Out.WriteLine(GetTotalRoute(5));
+                Console.Out.WriteLine(GetTotalRoute(6));
+                Console.Out.WriteLine(GetTotalRoute(7));
+                Console.Out.WriteLine(GetTotalRoute(8));
+                Console.Out.WriteLine(GetTotalRoute(9));
+                double value = 6;
+                for (double i = 3; i <= 20; i++)
+                {
+                    value = (3) * value + (i - 2) / i * value;
+                    Console.Out.WriteLine(value);
+                }
+            };
+
+            static int GetTotalRoute(int max)
+            {
+                var route = new Route(0, 0, max);
+                return route.Count + 1;
+            }
+
+            static int leftRow;
+            static int leftCol;
+            static int rightRow;
+            static int rightCol;
+            static int GetNextSteps(int row, int col, int max)
+            {
+                if (row == max && col == max)
+                    return 0;
+                if (row == max || col == max)
+                {
+                    return 1;
+                }
+
+                {
+                    leftRow = row;
+                    leftCol = col + 1;
+                    rightRow = row + 1;
+                    rightCol = col;
+                    return 2;
+                }
+
+            }
+
+
+            class Route
+            {
+                public Route(int row, int col, int max)
+                {
+                    var positions = GetNextSteps(row, col, max);
+                    var leftR = leftRow;
+                    var leftC = leftCol;
+                    var rightR = rightRow;
+                    var rightC = rightCol;
+                    if(positions == 2)
+                    {
+                        LeftRoute = new Route(leftR, leftC, max);
+                        RightRoute = new Route(rightR, rightC, max);
+                    }
+                }
+
+                Route LeftRoute { get; set; }
+                Route RightRoute { get; set; }
+
+                public int Count
+                {
+                    get
+                    {
+                        int count = 0;
+                        if(LeftRoute != null && LeftRoute != null)
+                        {
+                            ++count;
+                            count = count + LeftRoute.Count;
+                            count = count + RightRoute.Count;
+                        }
+
+                        return count;
+                    }
+                }
+
+            }
+
+            class Position
+            {
+                public Position(int row, int col)
+                {
+                    this.Row = row;
+                    this.Col = col;
+                }
+
+                public Position()
+                {
+                   
+                }
+
+                public int Row { get; set; }
+
+                public int Col { get;  set; }
+            }
+        }
+    }
+}
