23a2c784596b7254387509178912c44d141a4d7a
git_name <git_email>
2012-06-20 21:42:36 -0600

    problem 20, 21, 22

---
+++ code_kata/ProjectEuler/Problem20.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem20
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_figuring_problem_20 : concern
+        {
+            It should_get_correct_number = () =>
+            {
+                GetNumber(5).ShouldEqual(3);
+                Console.WriteLine(GetNumber(99));
+            };
+
+            static int GetNumber(int count)
+            {
+                var list = new List<int> { 1 };
+                for (int i = count + 1; i >= 2; i--)
+                {
+                    int j = 0;
+                    int tens = 0;
+                    while (j < list.Count)
+                    {
+                        var value = list[j] * (i -1);
+                        value += tens;
+                        tens = value / 10;
+                        if (tens > 0)
+                        {
+                            list[j] = value - tens * 10;
+                            if (j == list.Count - 1)
+                            {
+                                list.Add(0);
+                            }
+                        }
+                        else list[j] = value;
+
+                        j++;
+                    }
+                }
+
+
+                return list.Sum(x => x);
+            }
+
+        }
+    }
+}
---
+++ code_kata/ProjectEuler/Problem21.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+using System.Linq;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem21
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_observation_name : concern
+        {
+            It should_return_284_for_amicalble_of_220 = () =>
+            {
+                GetSecondAmicableNumbers(220).ShouldEqual(284);
+            };
+
+            It should_return_correct_result = () =>
+                Console.Out.WriteLine(GetSumOfAmicableNumber(10000));
+
+            static int GetSumOfAmicableNumber(int number)
+            {
+                var list = new List<int>();
+                for (int i = 1; i <= number; i++)
+                {
+                    if(list.Contains(i))
+                        continue;
+                    
+                    int secondAmicableNumbers = GetSecondAmicableNumbers(i);
+                    if( secondAmicableNumbers != 0 )
+                    {
+                        list.Add(i);
+                        list.Add(secondAmicableNumbers);
+                    }
+                }
+                return list.Sum(x => x);
+            }
+
+            static int GetSecondAmicableNumbers(int number)
+            {
+                int sumOfAmicableNumber = GetSumOfDivisors(number);
+                if (number != sumOfAmicableNumber && number == GetSumOfDivisors(sumOfAmicableNumber))
+                {
+                    return sumOfAmicableNumber;
+                }
+                return 0;
+            }
+
+            static int GetSumOfDivisors(int number)
+            {
+                var list = new List<int>();
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if(number % i == 0)
+                    {
+                        list.Add(i);
+                        if(!list.Contains(number / i))
+                        {
+                            list.Add(number/i);
+                        }
+
+                    }
+                }
+
+                return list.Sum(x => x) + 1;
+            }
+        }
+    }
+}
