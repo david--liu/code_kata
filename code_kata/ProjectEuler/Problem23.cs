280e145c1449a302937e8b4488a9e9c47a0b2cba
git_name <git_email>
2012-06-25 21:01:35 -0600

    Problem 23, 24

---
+++ code_kata/ProjectEuler/Problem23.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem23
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_observation_name : concern
+        {
+            It first_abundant_number_is_12 = () =>
+            {
+                for (int i = 1; i < 13; i++)
+                {
+                    if (GetSumOfDivisors(i) > i)
+                        i.ShouldEqual(12);
+                    
+                }
+            };
+
+            It should_get_the_correct_result = () =>
+            {
+                var map = GetAllAbundantNumbers();
+                var allPossible = new Dictionary<int, object>();
+                int sum = 0;
+
+                foreach (var key1 in map.Keys)
+                {
+                    foreach (var key2 in map.Keys)
+                    {
+                        var key = key1 + key2;
+                        if(!allPossible.ContainsKey(key))
+                            allPossible.Add(key, null);
+                    }
+                }
+
+                for (int i = 1; i < 28123; i++)
+                {
+                    if (!allPossible.ContainsKey(i))
+                        sum += i;
+                }
+
+                Console.Out.WriteLine(sum);
+
+            };
+
+            static Dictionary<int, object> GetAllAbundantNumbers()
+            {
+                var result = new Dictionary<int, object>();
+                for (int i = 12; i < 28123; i++)
+                {
+                    if(GetSumOfDivisors(i) > i)
+                    {
+                        result.Add(i, null);
+                    }
+                }
+
+                return result;
+            }
+
+            static int GetSumOfDivisors(int number)
+            {
+                var list = new List<int>();
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        list.Add(i);
+                        if (!list.Contains(number / i))
+                        {
+                            list.Add(number / i);
+                        }
+
+                    }
+                }
+
+                return list.Sum(x => x) + 1;
+            }
+ 
+        }
+    }
+}
---
+++ code_kata/ProjectEuler/Problem24.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+using System.Linq;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem24
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_getting_lexicographic_permutation : concern
+        {
+            It should_return2_for_1 = () =>
+            {
+                GetAllPermutations(new List<char> { '0', '1' }).Count.ShouldEqual(2);
+            };
+            It should_return6_for_2 = () =>
+            {
+                GetAllPermutations(new List<char> { '0', '1', '2' }).Count.ShouldEqual(6);
+            };
+
+            It should_get_correct_result = () =>
+            {
+                var allPermutations = GetAllPermutations(new List<char> {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'});
+                var stringList = new List<string>();
+                var stringBuilder = new StringBuilder();
+                foreach (var allPermutation in allPermutations)
+                {
+                    stringBuilder.Clear();
+
+                    foreach (var chr in allPermutation)
+                    {
+                        stringBuilder.Append(chr);
+                    }
+                    stringList.Add(stringBuilder.ToString());
+                }
+
+                stringList.Sort();
+
+                Console.Out.WriteLine(stringList[999999]);
+
+            };
+
+            static List<List<char>> GetAllPermutations(List<char> chars)
+            {
+                var list = new List<List<char>>();
+
+                if (chars.Count == 1)
+                {
+                    return new List<List<char>>{new List<char>{chars[0]}};
+                }
+
+                for (int i = 0; i < chars.Count; i++)
+                {
+                    var array = chars.Where(x => x != chars[i]).ToList();
+                    foreach (var permutation in GetAllPermutations(array))
+                    {
+                        permutation.Add(chars[i]);
+                        list.Add(permutation);
+                    }
+                }
+
+                return list;
+            }
+        }
+    }
+}
