4645cae111a74a1ba9d278afc8cdca0451bab165
git_name <git_email>
2012-08-16 16:55:17 -0600

    Problem 40

---
+++ code_kata/ProjectEuler/Problem40.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem40
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_observation_name : concern
+        {
+            It first_observation = () =>
+                Utils.PrintResult(() => BruteForce());
+        }
+
+        static int BruteForce()
+        {
+            int pointer = 9;
+            int result = 1*1;
+            for (int i = 10; i < 500000; i++)
+            {
+                var digits = MathUtils.ConvertToDigits(i);
+                result = Result(result, digits, pointer, 100);
+                result = Result(result, digits, pointer, 1000);
+                result = Result(result, digits, pointer, 10000);
+                result = Result(result, digits, pointer, 100000);
+                result = Result(result, digits, pointer, 1000000);
+                pointer += digits.Count;
+            }
+            return result;
+        }
+
+        static int Result(int result, List<int> digits, int pointer, int number)
+        {
+            if (pointer < number && (pointer + digits.Count) >= number)
+            {
+                result *= digits[number - pointer - 1];
+            }
+            return result;
+        }
+    }
+}
