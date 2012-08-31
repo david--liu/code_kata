cc9ea8a201d46831c0046128d55e9fe5749d9ae1
david liu <mrliuz@gmail.com>
2012-08-31 16:19:30 -0600

    Problem 74

---
+++ code_kata/ProjectEuler/Problem74.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using System.Linq;
+
+namespace code_kata.ProjectEuler
+{
+    public class Problem74
+    {
+        static Dictionary<int, SortedSet<int>> primeMap;
+
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_observation_name : concern
+        {
+            It first_observation = () =>
+            {
+                //BruteForce(8).ShouldEqual(3);
+                GetFactorial(145).ShouldEqual(145);
+            };
+
+            It should_get_result = () => Utils.PrintResult(() => BruteForce(1000000));
+
+        }
+
+        static int BruteForce(int limit)
+        {
+            int result = 0;
+
+            var map = new Dictionary<long, Dictionary<long, int>>();
+
+            for (int i = 69; i < limit; i++)
+            {
+                if(!map.ContainsKey(i))
+                {
+                    map.Add(i, new Dictionary<long, int>());
+                }
+
+                var f = GetFactorial(i);
+                while (!map[i].ContainsKey(f) && f != i)
+                {
+                    map[i].Add(f, 0);
+                    f = GetFactorial(f);
+                }
+
+                if(map[i].Count == 59)
+                {
+                    result ++;
+                }
+
+            }
+
+            
+
+            return result;
+        }
+
+        static long GetFactorial(long num)
+        {
+            return MathUtils.ConvertToDigits(num).Select(MathUtils.Factorial).Sum();
+        }
+    }
+}
